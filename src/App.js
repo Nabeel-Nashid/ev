df15ca4e67848441609fd15cf5432e490f8c5098
nabeelnashid <53267839+Nabeel-Nashid@users.noreply.github.com>
2023-08-12 20:53:40 +0530

    hosting

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import Navbar from "./pages/Navbar";
 import About from "./pages/About";
 import Service from "./pages/Service";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Products from "./pages/Products";
 
 function App() {
@@ -15,13 +14,7 @@ function App() {
       <About />
       <Service />
       <Products />
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/product" element={<Products />} />
-        </Routes>
-      </BrowserRouter> */}
+      
     </div>
   );
 }
